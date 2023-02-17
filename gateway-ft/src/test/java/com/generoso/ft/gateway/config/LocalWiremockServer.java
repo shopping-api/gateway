3d40fa1699e9dd5de0a6ff382c5e434039315ef1
Mauricio Generoso <mauro_generoso@hotmail.com>
2023-02-17 13:37:35 +0000

    Upgrade to Spring 3 and upgrade spring cloud version

--- gateway-ft/src/test/java/com/generoso/ft/gateway/config/LocalWiremockServer.java
+++ gateway-ft/src/test/java/com/generoso/ft/gateway/config/LocalWiremockServer.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
