bc27ee2c4fedc7d55a402e7dcbdef058964c00d9
Mauricio Generoso <mauro_generoso@hotmail.com>
2023-03-11 18:47:34 +0000

    upgrade to spring 3

--- gateway-app/src/main/java/com/generoso/gateway/GatewayApplication.java
+++ gateway-app/src/main/java/com/generoso/gateway/GatewayApplication.java
@@ -3,9 +3,7 @@ package com.generoso.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 // The class ReactiveUserDetailsServiceAutoConfiguration is removed because it generates a default user-password for security that is not necessary
 @SpringBootApplication(exclude = {ReactiveUserDetailsServiceAutoConfiguration.class})
 public class GatewayApplication {
