a0f67bd0e5166338faa2d17a0d0be26d6f8a2bdf
Mauricio Generoso <mauricio.generoso@hotmail.com>
2023-09-29 09:33:50 -0300

    Update security config because of deprecation

--- gateway-app/src/main/java/com/generoso/gateway/config/SecurityConfig.java
+++ gateway-app/src/main/java/com/generoso/gateway/config/SecurityConfig.java
@@ -2,25 +2,25 @@ package com.generoso.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.csrf().disable();
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http.csrf(CsrfSpec::disable);
 
-        http.authorizeExchange()
-                .pathMatchers("/private/**").permitAll()
-                .and()
-                .authorizeExchange()
-                .anyExchange()
-                .authenticated();
+        http.authorizeExchange(authorizeExchangeSpec -> {
+            authorizeExchangeSpec.pathMatchers("/private/**").permitAll();
+            authorizeExchangeSpec.anyExchange().authenticated();
+        });
 
-        http.oauth2Login();
-        http.oauth2Client();
+        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Client(Customizer.withDefaults());
         return http.build();
     }
 }
