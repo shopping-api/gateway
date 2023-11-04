5d8bc21df4278171a6325301519cfb5c2fd22c2f
Mauricio Generoso <mauricio.generoso@hotmail.com>
2023-11-04 22:03:23 +0000

    rename OpenApiConfiguration to OpenApiConfig

--- gateway-app/src/main/java/com/generoso/gateway/config/OpenApiConfig.java
+++ gateway-app/src/main/java/com/generoso/gateway/config/OpenApiConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfiguration {
+public class OpenApiConfig {
 
     @Autowired
     private BuildProperties buildProperties;
